e1b40583baa69ea1ccbfb3ffc38295d288df6eaf
Ezequiel Rodriguez <rodriguezsaneze55@gmail.com>
2024-06-02 22:13:03 -0300

    Modifique js

--- js/main.js
+++ js/main.js
@@ -18,32 +18,39 @@ function verInfoDeAfiliado(dni){
             alert ("El DNI ingresado no corresponde a un afiliado registrado")
         }
     }
+    console.log("Ingreso dniNuevo")
 }
 
 function verTodosLosAfiliados(){
     for (const afiliado of afiliadosGremio) {
         alert("DNI: " + afiliado.dni + " Nombre: " + afiliado.nombre + " Apellido: " + afiliado.apellido)
     }
+    console.log ("Ingreso opcion 3")
 }
 
 
 
 function main(){
     let continuar = confirm("Bienvenidos al registro y base de datos de afiliados")
+    console.log ("Bienvenido a la app de afiliados" )
     while(continuar){
         const accion = parseInt(prompt("Ingrese una opción: (1, 2 o 3)\n\n 1.Registrar afiliado\n 2.Ver afiliado\n 3.Ver todos los afiliados"))
+        console.log ("Eleccion de opciones")
         if(accion == "1"){
             const dniNuevo = prompt("Ingrese DNI del nuevo afiliado")
             const nombreNuevo = prompt("Ingrese nombre del afiliado")
             const apellidoNuevo = prompt("Ingrese apellido del afiliado")
             agregarNuevoAfiliado(dniNuevo,nombreNuevo,apellidoNuevo)
             verInfoDeAfiliado(dniNuevo)
+            // console.log (dniNuevo,nombreNuevo,apellidoNuevo)
         }else if(accion == "2"){
             const dniNuevo = prompt("Ingrese DNI del afiliado")
             verInfoDeAfiliado(dniNuevo)
+            console.log("Ingreso dniNuevo")
 
         }else if(accion =="3"){
             verTodosLosAfiliados()
+            console.log ("Ingreso opcion 3")
 
         }else{
             alert("Opción no válida")
